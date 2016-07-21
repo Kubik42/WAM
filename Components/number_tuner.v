0fd30f6c0dae03f44d075cbab08e026d9a8435f0
Kubik42 <dimakub@hotmail.com>
2016-07-21 17:50:18 -0400

    now works properly

--- Components/number_tuner.v
+++ Components/number_tuner.v
@@ -9,10 +9,10 @@ module tuner(
 	input [15:0] num,
 	input [15:0] min,
 	input [15:0] max,
-	output [15:0] tuned_num
+	output [16:0] tuned_num
 	);
 
-	wire [15:0] total = max - min;  // Total possibles numbers
-	wire [15:0] piece = ({16'd2}**power) / total;  // total*piece = 2^power
-	assign tuned_num = (({16'd2}**power) >= num) ? (num / piece) + min : 16'd0;
+	wire [16:0] total = max - min;  // Total possibles numbers
+	wire [16:0] piece = ({17'd2}**power) / total;  // total*piece = 2^power
+	assign tuned_num = (({17'd2}**power) >= num) ? (num / piece) + min : 17'd0;
 endmodule
