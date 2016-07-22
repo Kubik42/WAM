6b3a4e621506a924a69dd1dd89da534080b505fb
Kubik42 <dimakub@hotmail.com>
2016-07-22 15:07:48 -0400

    cleaned up

--- Controller/keypad_controller.v
+++ Controller/keypad_controller.v
@@ -13,102 +13,102 @@
 `include "../Components/key_down_register.v"
 
 module keypad_controller(
-	input [2:0] row,
-	input clk,
-	input reset,
-	output reg valid_key,
-	output [2:0] column,
-	output [3:0] key // Position of the button pressed (0-8)
-	);
+    input [2:0] row,
+    input clk,
+    input reset,
+    output reg valid_key,
+    output [2:0] column,
+    output [3:0] key // Position of the button pressed (0-8)
+    );
 
-	// Clock dividers ----------------------------------------------------------
+    // Clock dividers ----------------------------------------------------------
 
-	wire clk_1MHz, clk_183Hz, clk_31Hz;  // Scan rate is approx six times slower than the debounce clock
-	wire [5:0] counter_1Mhz;
-	wire [12:0] counter_183Hz;
-	wire [14:0] counter_31Hz;
-	localparam [5:0]  counter_max_1MHz = 6'd49,       // Relative to 50Mhz
-			   [12:0] counter_max_183Hz = 13'd5464  // Relative to 1MHz
-			   [14:0] counter_max_31Hz = 15'd32257;  // Relative to 1MHz			  
+    wire clk_1MHz, clk_183Hz, clk_31Hz;  // Scan rate is approx six times slower than the debounce clock
+    wire [5:0] counter_1Mhz;
+    wire [12:0] counter_183Hz;
+    wire [14:0] counter_31Hz;
+    localparam [5:0]  counter_max_1MHz = 6'd49,       // Relative to 50Mhz
+               [12:0] counter_max_183Hz = 13'd5464  // Relative to 1MHz
+               [14:0] counter_max_31Hz = 15'd32257;  // Relative to 1MHz              
 
-	clock_divider CD_1Hz(.counter_max(counter_max_1Hz),
-						 .clk(clk),
-					 	 .enable(1'b1),
-					 	 .reset(reset),
-					 	 .counter(counter_1MHz));
+    clock_divider CD_1Hz(.counter_max(counter_max_1Hz),
+                         .clk(clk),
+                         .enable(1'b1),
+                         .reset(reset),
+                         .counter(counter_1MHz));
 
-	assign clk_1MHz = (counter_1MHz == 6'd0) ? 1 : 0;
+    assign clk_1MHz = (counter_1MHz == 6'd0) ? 1 : 0;
 
-	clock_divider CD_183Hz(.counter_max(counter_max_183Hz),
-						   .clk(clk_1Hz),
-					 	   .enable(1'b1),
-					 	   .reset(reset),
-					 	   .counter(counter_183Hz));
+    clock_divider CD_183Hz(.counter_max(counter_max_183Hz),
+                           .clk(clk_1Hz),
+                           .enable(1'b1),
+                           .reset(reset),
+                           .counter(counter_183Hz));
 
-	assign clk_183Hz = (counter_183Hz == 13'd0) ? 1 : 0; 
+    assign clk_183Hz = (counter_183Hz == 13'd0) ? 1 : 0; 
 
-	clock_divider CD_31Hz(.counter_max(counter_max_31Hz),
-						  .clk(clk_1Hz),
-						  .enable(1'b1),
-						  .reset(reset),
-					 	  .counter(counter_31Hz));
+    clock_divider CD_31Hz(.counter_max(counter_max_31Hz),
+                          .clk(clk_1Hz),
+                          .enable(1'b1),
+                          .reset(reset),
+                          .counter(counter_31Hz));
 
-	assign clk_31Hz = (counter_31Hz == 15'd0) ? 1 : 0; 
+    assign clk_31Hz = (counter_31Hz == 15'd0) ? 1 : 0; 
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	// Key debouncer
-	debouncer DEB(.row(row),
-				.clk(clk_183Hz),
-				.key_down(key_down));
+    // Key debouncer
+    debouncer DEB(.row(row),
+                .clk(clk_183Hz),
+                .key_down(key_down));
 
-	// Key register
-	keyreg KEYREG(.pressed({row_key, column_key}),
-						.clk(key_down),
-						.reset(reset),
-						.key(key));
+    // Key register
+    keyreg KEYREG(.pressed({row_key, column_key}),
+                        .clk(key_down),
+                        .reset(reset),
+                        .key(key));
 
-	// Valid key register
-	valkeyreg VALKEYREG(.clk(key_down),
-						.reset(),
-						.valid_key(valid_key));
+    // Valid key register
+    valkeyreg VALKEYREG(.clk(key_down),
+                        .reset(),
+                        .valid_key(valid_key));
 
-	// Counter
-	wire [1:0] counter;  // 2-bit counter
+    // Counter
+    wire [1:0] counter;  // 2-bit counter
 
-	counter C(.clk(clk_31Hz),
-			  .counter(counter));
+    counter C(.clk(clk_31Hz),
+              .counter(counter));
 
 
-	// 3-bit to 2-bit Encoder and 2-bit to 3-bit decoder
-	wire [1:0] row_key;
-	wire [1:0] column_key;
+    // 3-bit to 2-bit Encoder and 2-bit to 3-bit decoder
+    wire [1:0] row_key;
+    wire [1:0] column_key;
 
-	encoder ENC(.row(~row),
-				.key(row_key));
+    encoder ENC(.row(~row),
+                .key(row_key));
 
-	decoder DEC(.in(counter),
-				.column(column_key));
+    decoder DEC(.in(counter),
+                .column(column_key));
 
-	assign column = ~column_key;
-	assign key = ((2'd3)*{2'b00, row_key} + {2'b00, column_key}); 
+    assign column = ~column_key;
+    assign key = ((2'd3)*{2'b00, row_key} + {2'b00, column_key}); 
 endmodule
 
 // 2 bit synchronous counter, resets at 11
 module counter(
-	input clk,
-	output reg [1:0] counter
-	);
+    input clk,
+    output reg [1:0] counter
+    );
 
-	tff F0(.data(1'b1),
-		   .clk(clk),
-		   .reset(~(counter[0] & counter[1])),
-		   .Q(counter[0]));
+    tff F0(.data(1'b1),
+           .clk(clk),
+           .reset(~(counter[0] & counter[1])),
+           .Q(counter[0]));
 
-	tff F1(.data(1'b1 & counter[0]),
-		   .clk(clk),
-		   .reset((~(counter[0] & counter[1])),
-		   .Q(counter[1]));
+    tff F1(.data(1'b1 & counter[0]),
+           .clk(clk),
+           .reset((~(counter[0] & counter[1])),
+           .Q(counter[1]));
 
-	//assign counter = Q;
+    //assign counter = Q;
 endmodule
--- wam.v
+++ wam.v
@@ -9,157 +9,157 @@
 // SW[5]   - total game points
 
 module wam(
-	input [0:0] KEY,
-	input [9:0] SW,
-	input CLOCK_50,
-	input [2:0] key_matrix_row,
-	output reg [8:0] LEDR
-	);
+    input [0:0] KEY,
+    input [9:0] SW,
+    input CLOCK_50,
+    input [2:0] key_matrix_row,
+    output reg [8:0] LEDR
+    );
 
-	assign play = ~KEY[0];
-	reg load_seed, reset;
+    assign play = ~KEY[0];
+    reg load_seed, reset;
 
-	// States ------------------------------------------------------------------
+    // States ------------------------------------------------------------------
 
-	wire [1:0] current_state, next_state;
+    wire [1:0] current_state, next_state;
 
-	localparam SETUP   = 2'd0,
-			PLAY    = 2'd1,
-			RESTART = 2'd2;
+    localparam SETUP   = 2'd0,
+            PLAY    = 2'd1,
+            RESTART = 2'd2;
 
-	always @(*)
-	begin: state_table
-		case(current_state)
-			SETUP: next_state = play ? PLAY : SETUP;
-			PLAY: next_state = play ? RESTART : PLAY;
-			RESTART: next_state = PLAY;
-		endcase
-	end
+    always @(*)
+    begin: state_table
+        case(current_state)
+            SETUP: next_state = play ? PLAY : SETUP;
+            PLAY: next_state = play ? RESTART : PLAY;
+            RESTART: next_state = PLAY;
+        endcase
+    end
 
-	always @(*)
-	begin: game_setup
-		// By default
-		load_seed = 1'b0;
-		reset = 1'b0;
-		start_light = 1'b0;
+    always @(*)
+    begin: game_setup
+        // By default
+        load_seed = 1'b0;
+        reset = 1'b0;
+        start_light = 1'b0;
 
-		case(current_state)
-			SETUP: begin
-				load_seed = 1'b1;  // Seed is loaded only once
-				reset = 1'b1;
-				start_light = 1'b0;
-			end
-			PLAY: begin
-				load_seed = 1'b0;
-				reset = 1'b1;
-				start_light = 1'b1;
-			end
-			RESTART: begin
-				load_seed = 1'b0;
-				reset = 1'b0;
-				start_light = 1'b0;
-			end
-		endcase
-	end
+        case(current_state)
+            SETUP: begin
+                load_seed = 1'b1;  // Seed is loaded only once
+                reset = 1'b1;
+                start_light = 1'b0;
+            end
+            PLAY: begin
+                load_seed = 1'b0;
+                reset = 1'b1;
+                start_light = 1'b1;
+            end
+            RESTART: begin
+                load_seed = 1'b0;
+                reset = 1'b0;
+                start_light = 1'b0;
+            end
+        endcase
+    end
 
-	always @(posedge CLOCK_50 or negedge play)
-	begin: game
-		if (play) begin
-			current_state <= RESTART;
-		end
-		else begin
-			current_state <= next_state;
-		end
-	end
+    always @(posedge CLOCK_50 or negedge play)
+    begin: game
+        if (play) begin
+            current_state <= RESTART;
+        end
+        else begin
+            current_state <= next_state;
+        end
+    end
 
-	// Basic game settings -----------------------------------------------------
+    // Basic game settings -----------------------------------------------------
 
-	// Switches
-	wire [3:0] difficulty;
-	assign difficulty = SW[3:0]
+    // Switches
+    wire [3:0] difficulty;
+    assign difficulty = SW[3:0]
 
-	wire [3:0] gamemode;
-	assign gamemode = SW[9:6];
+    wire [3:0] gamemode;
+    assign gamemode = SW[9:6];
 
-	// Points
-	localparam [4:0] normal_points = 5'd25,    // 25 light flicks
-			   [5:0] extended_points = 6'd50;  // 50 light flicks
-	reg [5:0] total_points;
+    // Points
+    localparam [4:0] normal_points = 5'd25,    // 25 light flicks
+               [5:0] extended_points = 6'd50;  // 50 light flicks
+    reg [5:0] total_points;
 
-	// Counters/timers
-	reg [27:0] time_between;  // Time between subsequent light flicks
-	reg [27:0] time_on;  // Time the light will stay on for
+    // Counters/timers
+    reg [27:0] time_between;  // Time between subsequent light flicks
+    reg [27:0] time_on;  // Time the light will stay on for
 
-	always @(*) 
-	begin : Difficulty
-		case (difficulty)
-			0001: begin  // Level 1: 2 seconds (count up to 100_000_000 - 1) 
-				time_between <= 28'd99_999_999;
-				time_on <= 28'd99_999_999;
-			end
-			0010: begin  // Level 2: 1 second
-				time_between <= 28'd49_999_999;
-				time_on <= 28'd49_999_999;
-			end
-			0100: begin  // Level 3: 0.50 seconds (count up to 25_000_000 - 1), 1 second countdown
-				time_between <= 28'd24_999_999;
-				time_on <= 28'd49_999_999;
-			end
-			1000: begin  // Level 4: 0.25 seconds (count up to 12_500_000 - 1), 0.50 second countdown
-				time_between <= 28'd12_499_999;
-				time_on <= 28'd24_999_999;
-			end
-			default: begin  // Same as Level 2: 1 second
-				time_between <= 28'd49_999_999;
-				time_on <= 28'd49_999_999;
-			end
-		endcase
-	end
+    always @(*) 
+    begin : Difficulty
+        case (difficulty)
+            0001: begin  // Level 1: 2 seconds (count up to 100_000_000 - 1) 
+                time_between <= 28'd99_999_999;
+                time_on <= 28'd99_999_999;
+            end
+            0010: begin  // Level 2: 1 second
+                time_between <= 28'd49_999_999;
+                time_on <= 28'd49_999_999;
+            end
+            0100: begin  // Level 3: 0.50 seconds (count up to 25_000_000 - 1), 1 second countdown
+                time_between <= 28'd24_999_999;
+                time_on <= 28'd49_999_999;
+            end
+            1000: begin  // Level 4: 0.25 seconds (count up to 12_500_000 - 1), 0.50 second countdown
+                time_between <= 28'd12_499_999;
+                time_on <= 28'd24_999_999;
+            end
+            default: begin  // Same as Level 2: 1 second
+                time_between <= 28'd49_999_999;
+                time_on <= 28'd49_999_999;
+            end
+        endcase
+    end
 
-	always @(*)
-	begin: Game mode
-		case (gamemode)
-			0001: begin  // Normal
-				
-			end
-			0010: begin  // Timed
-				
-			end
-			0100: begin  // Deathmatch (1 miss = you lose)
-				
-			end
-			1000: begin  // Level continuity (start from 0 go to 4)
-				
-			end
-			default: begin  // Same as normal
-				
-			end
-		endcase
-	end
+    always @(*)
+    begin: Game mode
+        case (gamemode)
+            0001: begin  // Normal
+                
+            end
+            0010: begin  // Timed
+                
+            end
+            0100: begin  // Deathmatch (1 miss = you lose)
+                
+            end
+            1000: begin  // Level continuity (start from 0 go to 4)
+                
+            end
+            default: begin  // Same as normal
+                
+            end
+        endcase
+    end
 
-	always @(*)
-	begin: Game points
-		case(SW[5])
-			0: total_points <= normal_points;
-			1: total_points <= extended_points;
-			default: total_points <= normal_points;
-		endcase
-	end
+    always @(*)
+    begin: Game points
+        case(SW[5])
+            0: total_points <= normal_points;
+            1: total_points <= extended_points;
+            default: total_points <= normal_points;
+        endcase
+    end
 
-	// -------------------------------------------------------------------------
-	
-	// Light controller
-	light_controller LC(.time_on(time_on),
-						.time_between(time_between),
-						.load_seed(load_seed),
-						.start(start_light)
-						.clk(CLOCK_50),
-						.reset(reset),
-						.lights(LEDR));
+    // -------------------------------------------------------------------------
+    
+    // Light controller
+    light_controller LC(.time_on(time_on),
+                        .time_between(time_between),
+                        .load_seed(load_seed),
+                        .start(start_light)
+                        .clk(CLOCK_50),
+                        .reset(reset),
+                        .lights(LEDR));
 
-	// Keypad controller
-	keypad_controller KC(.row(key_matrix_row),
-						 .clk(CLOCK_50),
-						 .reset);
+    // Keypad controller
+    keypad_controller KC(.row(key_matrix_row),
+                         .clk(CLOCK_50),
+                         .reset);
 
 endmodule
