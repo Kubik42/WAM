e60b07e1c00ee1db7e0698941534b6f363f7d985
Kubik42 <dimakub@hotmail.com>
2016-07-22 14:52:29 -0400

    now correctly produces max valueclear

--- Components/simple_number_tuner.v
+++ Components/simple_number_tuner.v
@@ -1,6 +1,6 @@
 // Converts the given number to a new number within the interval 0-8
 //
-// NOTE: given number must be <= 2^16
+// NOTE: given number must be < 2^16
 
 `timescale 1ns / 1ns // `timescale time_unit/time_precision
 
@@ -9,6 +9,6 @@ module tuner(
 	output [16:0] tuned_num
 	);
 
-	localparam [16:0] piece = 17'd65536 / 17'd8;
-	assign tuned_num = num / piece;	
+	localparam [16:0] piece = 17'd65536 / 17'd9;
+	assign tuned_num = (num >= 17'd65529) ? 17'd8 : num / piece;	
 endmodule
