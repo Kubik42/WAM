0e88296230d2e2a0189043caeb3d5dc340eb54f6
Kubik42 <dimakub@hotmail.com>
2016-07-23 20:49:31 -0400

    cleaned up

--- Components/valid_key_register.v
+++ Components/valid_key_register.v
@@ -3,15 +3,15 @@
 `timescale 1ns / 1ns // `timescale time_unit/time_precision
 
 module valkeyreg(
-	input clk,
-	input reset,
-	output valid_key
-	);
+    input clk,
+    input reset,
+    output reg valid_key
+    );
 
-	always @(posedge clk or negedge reset) begin
-		if (~reset)
-			valid_key <= 1'b0;
-		else
-			valid_key <= 1'b1;
-	end
+    always @(posedge clk or negedge reset) begin
+        if (~reset)
+            valid_key <= 1'b0;
+        else
+            valid_key <= 1'b1;
+    end
 endmodule
