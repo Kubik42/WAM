9b1a9ebf0670091c6a56994b54ebfaa75dfdfc87
Kubik42 <dimakub@hotmail.com>
2016-08-07 16:17:22 -0400

    keys now go in row order

--- Components/key_decoder.v
+++ Components/key_decoder.v
@@ -11,13 +11,13 @@ module key_decoder(
     begin
         case (key)
             4'b0000: key_number <= 4'd0;
-            4'b0100: key_number <= 4'd1;
-            4'b1000: key_number <= 4'd2;
-            4'b0001: key_number <= 4'd3;
+            4'b0001: key_number <= 4'd1;
+            4'b0010: key_number <= 4'd2;
+            4'b0100: key_number <= 4'd3;
             4'b0101: key_number <= 4'd4;
-            4'b1001: key_number <= 4'd5;
-            4'b0010: key_number <= 4'd6;
-            4'b0110: key_number <= 4'd7;
+            4'b0110: key_number <= 4'd5;
+            4'b1000: key_number <= 4'd6;
+            4'b1001: key_number <= 4'd7;
             4'b1010: key_number <= 4'd8;
             default: key_number <= 4'd15;  // This key doesn't exist
         endcase
